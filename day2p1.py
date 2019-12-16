2b6dcbeab411032dfe5494c02958788cd07ae0c1
patrick.fischer <patrick.fischer@ewz.ch>
2019-12-16 19:57:53 +0100

    d2p1 done

--- day2p1.py
+++ day2p1.py
@@ -28,11 +28,20 @@ def compute(arg: List[int]) -> List[int]:
             arg[resIdx] = arg[val1idx] * arg[val2idx]
         else:
             raise Exception('no valid opcode')
-        print(arg)
     return arg
 
 
-assert np.array_equal(compute([1, 0, 0, 0, 99]), [2, 0, 0, 0, 99])
-assert np.array_equal(compute([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99])
-assert np.array_equal(compute([2,4,4,5,99,0]), [2,4,4,5,99,9801])
-assert np.array_equal(compute([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+# assert np.array_equal(compute([1, 0, 0, 0, 99]), [2, 0, 0, 0, 99])
+# assert np.array_equal(compute([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99])
+# assert np.array_equal(compute([2,4,4,5,99,0]), [2,4,4,5,99,9801])
+# assert np.array_equal(compute([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+
+with open('day02-Input.txt') as f:
+    instr = f.readline().strip().split(',')
+    # print(instr.split(','))
+    inval = list(map(int, instr))
+    inval[1] = 12
+    inval[2] = 2
+    # print(inval)
+    res = compute(inval)
+print(res[0])
