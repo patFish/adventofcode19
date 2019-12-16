cb91eed605070bc3222411d9c97ddda75c3d84d7
patrick.fischer <patrick.fischer@ewz.ch>
2019-12-16 13:12:17 +0100

    first def

--- 19/day01.py
+++ 19/day01.py
@@ -8,3 +8,12 @@ For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel re
 For a mass of 1969, the fuel required is 654.
 For a mass of 100756, the fuel required is 33583.
 '''
+
+
+def fuel(mass: int) -> int:
+    return mass//3 - 2
+
+
+assert fuel(12) == 2
+assert fuel(14) == 2
+assert fuel(1969) == 654
